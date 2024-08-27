8bb49c892cc5788feba03a8a1b6250ee1e090b2f
Dhruvin Patel <dhruvinvpatel12@gmail.com>
2024-08-26 20:05:56 -0400

    modified changes and removed error

--- weather.py
+++ weather.py
@@ -1,29 +1,30 @@
 from dotenv import load_dotenv
 from pprint import pprint
 import requests
-import os 
+import os
 
 load_dotenv()
 
-def get_current_weather(city="Oshawa"):
 
-    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
- 
+def get_current_weather(city="Toronto"):
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
     weather_data = requests.get(request_url).json()
 
     return weather_data
+
 
 if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nPlease enter a city name: ")
 
-    # check for empty strings or string with only spaces
+    # Check for empty strings or string with only spaces
     if not bool(city.strip()):
         city = "Toronto"
 
     weather_data = get_current_weather(city)
 
     print("\n")
-
     pprint(weather_data)
